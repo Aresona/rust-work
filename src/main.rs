5dacffe1f6030f623a77d61af8447cca4591d2a0
binbin <binbin@ozi-tech.com>
2022-07-16 17:11:42 +0800

    temp

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,60 @@ enum Shape<T> {
     Triangle { a: T, b: T, c: T },
 }
 
+// fn area<
+//     T: Mul<Output = T> + Copy + From<f64> + Sub<Output = T> + Add<Output = T> + Div<Output = T>,
+// >(
+//     shape: Shape<T>,
+// ) -> f64 {
+//     match shape {
+//         Shape::Circle { radius } => {
+//             let temp = radius * radius;
+//             std::f64::consts::PI * temp.into()
+//         }
+//         Shape::Square { line } => (line * line).into(),
+//         Shape::Triangle { a, b, c } => {
+//             let semiperimeter = (a + b + c) / 2.0;
+//             (semiperimeter.into()
+//                 * (semiperimeter.into() - a.into())
+//                 * (semiperimeter.into() - b.into())
+//                 * (semiperimeter.into() - c.into()))
+//             .sqrt()
+//         }
+//     }
+// }
+
 fn area<
     T: Mul<Output = f64>
         + Copy
-        + From<f64>
+        + Sub<f64, Output = f64>
         + Sub<T, Output = f64>
-        + Add<T, Output = f64>
-        + Div<T, Output = f64>,
+        + Mul<f64, Output = f64>
+        + Add<Output = T>
+        + From<f64>,
 >(
     shape: Shape<T>,
 ) -> f64 {
     match shape {
-        // Shape::Circle { radius } => {
-        //     let radius: f64 = radius.into();
-        //     std::f64::consts::PI * radius * radius
-        // }
-        Shape::Square { line } => (line * line).into(),
-        Shape::Circle { radius } => (radius * radius).into(),
-        // Shape::Triangle { a, b, c } => a * a * a,
+        Shape::Circle { radius } => {
+            let temp = radius * radius;
+            std::f64::consts::PI * temp
+        }
+        Shape::Square { line } => line * line,
         Shape::Triangle { a, b, c } => {
-            let semiperimeter: f64 = (a + b + c).into() / 2.0;
-            semiperimeter.sqrt()
-            // (semiperimeter.into()
-            //     * (semiperimeter.into() - a.into())
-            //     * (semiperimeter.into() - b.into())
-            //     * (semiperimeter.into() - c.into()))
-            // .sqrt()
+            let semiperimeter = (a + b + c) * 0.5;
+            (semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c)).sqrt()
         }
     }
 }
+
 fn main() {
-    let triangle = Shape::Triangle::<f64> {
+    let triangle = Shape::Triangle {
         a: 1.0,
         b: 3.1,
         c: 2.3,
     };
-    let circle = Shape::Circle { radius: 2.1 };
-    let square = Shape::Square { line: 3.3 };
+    let circle = Shape::Circle { radius: 4.1 };
+    let square = Shape::Square { line: 3.2 };
     let triangle_area = area(triangle);
     let square_area = area(square);
     let circle_area = area(circle);
